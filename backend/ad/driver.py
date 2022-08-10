01231c2a1b8efeb726890d9535a74a035b638ef9
motionrus <tytin@yandex.ru>
2022-08-11 01:03:28 +0300

    increase performance upload

--- backend/ad/driver.py
+++ backend/ad/driver.py
@@ -9,15 +9,15 @@ from selenium import webdriver
 
 
 def try_to_get(func):
-    count_tries = 5
-    while count_tries:
+    count_tries = 0
+    while True:
         time.sleep(5)
         try:
             return func()
         except Exception as err:
-            print(f"Count Tries Left: {count_tries}")
+            print(f"Count Tries: {count_tries}")
             print(err)
-        count_tries -= 1
+        count_tries += 1
 
 
 def get_session():
@@ -51,7 +51,14 @@ options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=options)
 driver.quit()
 
+
+if int(os.getenv("WORKER", 0)):
+    print("WAITING START BACKEND...")
+    try_to_get(lambda: requests.get('http://backend:8000/api/grab/'))
+
+
 session = try_to_get(get_session)
+
 
 if session:
     driver.command_executor._url = selenium_url.geturl()
